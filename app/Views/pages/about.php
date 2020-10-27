6e7753045b82d35fc101303bd477e59912d49ddc
farhanpw <farhan.fpw@gmail.com>
2020-10-27 17:54:31 +0700

    Myth/Auth_login

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ipsum ex repellat delectus veritatis voluptatem et blanditiis nemo omnis laborum. Dolor quia recusandae nobis perferendis obcaecati. Ipsum numquam suscipit debitis!</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
